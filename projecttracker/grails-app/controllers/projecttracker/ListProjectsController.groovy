dd51fc2e4355ef999a5228e6b05aaf2c96088a25
luari <luarinblue@gmail.con>
2013-03-22 18:13:32 -0300

    test

---
+++ projecttracker/grails-app/controllers/projecttracker/ListProjectsController.groovy
@@ -0,0 +1,16 @@
+package projecttracker
+
+class ListProjectsController {
+
+    def index = { 
+		redirect (action: current)
+	}
+	
+	def current = {
+		render "<h1>Project Tracker Video Tutorials</h1>"
+	}
+	
+	def overdue = {
+		render "Order Valentines Day Package"
+	}
+}
---
+++ projecttracker/grails-app/domain/projecttracker/Modelocelular.groovy
@@ -0,0 +1,10 @@
+package projecttracker
+
+class Modelocelular {
+
+	String nombre
+	
+    static constraints = {
+		nombre(size:1..25)
+    }
+}
